fedc878e131d492b1b58e0a5910d1b545d1ebb29
lzh65038-web <lzh65038@gmail.com>
2026-04-20 23:28:00 +0800

    4.20
    链表算法题1

---
+++ 4.20/4.20/4.20.cpp
@@ -0,0 +1,74 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* removeElements(struct ListNode* head, int val)
+{
+    //思路一：创建两个指针
+    //先处理头节点
+    while (head && head->val == val)
+    {
+        //更改头节点
+        head = head->next;
+    }
+
+    struct ListNode* cur = head;
+    struct ListNode* prev = head;
+
+    while (cur)
+    {
+        if (cur->val == val)
+        {
+            //删除cur
+            struct ListNode* next = cur->next;
+            prev->next = cur->next;
+            free(cur);
+            cur = next;
+        }
+        else
+        {
+            //同时向后走
+            prev = cur;
+            cur = cur->next;
+        }
+    }
+    return head;
+}
+
+
+struct ListNode* removeElements(struct ListNode* head, int val)
+{
+    //思路二 
+    struct ListNode* cur = head;
+    struct ListNode* newhead = NULL;
+    struct ListNode* newtail = NULL;
+
+    while (cur)
+    {
+        if (cur->val != val)
+        {
+            //加入到新链表中
+            if (newhead == NULL)
+            {
+                //新链表的第一个节点，既是头节点，也是尾节点
+                newhead = newtail = cur;
+            }
+            else
+            {
+                //尾插即可
+                newtail->next = cur;
+                newtail = cur;
+            }
+        }
+        cur = cur->next;
+    }
+    if (newtail != NULL) newtail->next = NULL;
+
+    return newhead;
+}
+
+
